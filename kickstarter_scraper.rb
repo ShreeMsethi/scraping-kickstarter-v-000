84ade679e740741fbb91788f9bed697da438198d
Shreemangal Sethi <shreemangal.sethi@gmail.com>
2020-08-12 05:41:14 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,4 +1,10 @@
 # require libraries/modules here
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+
 require 'nokogiri'
 require 'pry'
 
@@ -12,6 +18,3 @@ end
 
 create_project_hash
 
-# projects: kickstarter.css("li.project.grid_4")
-# title: project.css("h2.bbcard_name strong a").text
-# image link: project.css("div.project-thumbnail a img").attribute("src").value
